d8db0d21494c30e752f553bf7d102957a506df5f
daegeunkim92 <daegeunkim92@gmail.com>
2020-09-03 10:52:49 +0800

    leave, init_room

--- public/javascripts/socket_client.js
+++ public/javascripts/socket_client.js
@@ -1,51 +1,103 @@
 var socket = io();
 var isInRoom = false
+var room_id = -1
+const msg_join_fail = 'You\'re already in the room'
+const msg_welcome = 'Welcome to room'
+const msg_full = 'This room is already full'
+const msg_leave = ' leave this room'
+
+window.onload = () => {
+    document.getElementById('btnCreateRoom').onclick = () => {
+        if(room_id >= 0){
+            alert(msg_join_fail + room_id)
+            return
+        }
+        else{
+            socket.emit('create_room')
+        }
+    }
+}
 
 socket.on('connect', () => {
     console.log('connect to socket')
 })
 
 socket.on('create_room', (data) => {
-    console.log(data)
-    let btnNewRoom = document.createElement('button')
-    btnNewRoom.innerHTML = '<p>'+ 'room' + data.room_id + '</p>'
-    btnNewRoom.innerHTML += '<p id=' + data.room_id + '>'+ 'member_num : ' + data.members.length + '</p>'
-    btnNewRoom.onclick = () => {
-        socket.emit('join_room', data.room_id)
+    if(data.members[0] === socket.id){
+        room_id = data.room_id
     }
+    
+    let btnNewRoom = createRoomButton(data.room_id, 1)
     document.getElementById('divRoomList').appendChild(btnNewRoom)
 })
 
 socket.on('join_room', (data) => {
-    console.log(data)
-    document.getElementById(data).innerText = 'this room is full'
-})
-
-socket.on('new_member', (data) => {
-    console.log(data)
     if(data.result){
-        if(data.socket_id !== socket.id){
-            alert('Here comes a new member')
+        document.getElementById(data.room_id).innerText = 'this room is full'
+
+        if(data.socket_id === socket.id){
+            room_id = data.room_id
         }
-        else{
-            isInRoom = true
+
+        if(data.room_id === room_id){
+            if(data.socket_id === socket.id){
+                alert(msg_welcome + data.room_id)
+            }
+            else{
+                alert(socket.id + ' comes')
+            }
         }
     }
     else{
-        if(data.socket_id === socket.id)
-        alert('You can\'t enter this room')
+        if(data.socket_id === socket.id){
+            alert(msg_full)
+        }
     }
 })
 
-window.onload = () => {
-    document.getElementById('btnCreateRoom').onclick = () => {
-        if(isInRoom){
-            alert('You already join on room')
+socket.on('leave_room', (data) => {
+    console.log(data)
+
+    if(data.isRoomDeleted){
+        deleteRoomButton(data.room_id)
+    }
+    else{
+        document.getElementById(data.room_id).innerHTML = '<p id=' + data.room_id + '>'+ 'member_num : 1' +  '</p>'
+    }
+
+    if(room_id === data.room_id){
+        alert(data.socket_id + msg_leave)
+    }
+})
+
+socket.on('init_room_info', (data) => {
+    for(let i in data){
+        let btnNewRoom = createRoomButton(data[i].room_id, data[i].members.length)
+        document.getElementById('divRoomList').appendChild(btnNewRoom)
+    }
+})
+
+function createRoomButton(_room_id, _member_num){
+    let btnNewRoom = document.createElement('button')
+    btnNewRoom.innerHTML = '<p>'+ 'room' + _room_id + '</p>'
+    if(_member_num === 1){
+        btnNewRoom.innerHTML += '<p id=' + _room_id + '>'+ 'member_num : ' + _member_num + '</p>'
+    }
+    else{
+        btnNewRoom.innerHTML += '<p id=' + _room_id + '>'+ 'this room is full' + '</p>'
+    }
+    //add join event
+    btnNewRoom.onclick = () => {
+        if(room_id >= 0){
+            alert(msg_join_fail + room_id)
             return
         }
-        else{
-            socket.emit('create_room')
-            isInRoom = true
-        }
+        socket.emit('join_room', _room_id)
     }
+    return btnNewRoom
+}
+
+function deleteRoomButton(_room_id){
+    let btnSelected = document.getElementById(_room_id).parentElement
+    document.getElementById('divRoomList').removeChild(btnSelected)
 }
--- socket_app.js
+++ socket_app.js
@@ -4,15 +4,8 @@ var room_info = []
 
 io.on('connect', (socket) => {
     console.log('socket_app connected')
-    //socket.send('hello') 1:1 send
-    //1. 접속하자마자 현재 존재하는 방들의 목록을 보여줘야 한다.
-    //2. 사실상 이 기능은 방정보가 캐시에 저장되어 있으면 로딩시 보여주는게 맞다.
+
     socket.on('create_room', () => {
-        //0. 방이 아예 없다면 ID를 1로 해서 방을 새로 만든다.
-        //1. 가장 마지막 방의 ID를 가져옴
-        //2. 해당 ID + 1해서 새롭게 방을 만듬
-        //3. 해당 방에 해당 인원을 JOIN시킴
-        //4. 모두에게 방이 새로 생겼음을 알리고 방 번호도 알려줌
         let room_len = room_info.length
         let new_room_id = 1
 
@@ -22,7 +15,7 @@ io.on('connect', (socket) => {
 
         let new_room = {
             room_id: new_room_id,
-            members: [socket.id]
+            members: [socket.id],
         }
 
         room_info.push(new_room)
@@ -33,17 +26,8 @@ io.on('connect', (socket) => {
     })
 
     socket.on('join_room', (data) => {
-        //1. 들어갈 방번호를 받음
-        //2. 해당 방에 있는 인원 수가 1명이면
-        //2.1. 방을 만들고
-        //2.2. 해당 방에 인원을 JOIN시킴
-        //2.3. 방에 있는 인원들에게 '입장했다는 것을 알려줌'
-        //3. 이미 두 명이면 불가능
         let result = false
         for(let i in room_info){
-            console.log(room_info[i])
-            console.log(socket.id)
-            console.log(data)
             if(room_info[i].room_id === data){
                 if(room_info[i].members.length === 1){
                     if(room_info[i].members[0] === socket.id){
@@ -56,26 +40,54 @@ io.on('connect', (socket) => {
                 break
             }
         }
-        
-        console.log(result)
-        if(result){
-            io.emit('join_room', data)
-        }
 
-        io.to('room' + data).emit('new_member', {
+        io.emit('join_room',{
             result: result,
-            socket_id: socket.id
+            socket_id: socket.id,
+            room_id: data
         })
     })
 
     socket.on('disconnect', () => {
         console.log('socket_app disconnected')
-        //1. 얘가 속해있던 방을 찾는다.
-        //2. 그 방에서 얘를 뺀다.
-        //3. 만약 얘를 빼고 인원수가 0이 된다면
-        //3.1. 방을 없앤다.
-        //3.2. 방이 없어졌음을 모두에게 알린다.
+
+        console.log(room_info)
+
+        var isRoomDeleted = false
+
+        for(var i in room_info){
+            let flag = false
+            for(var j in room_info[i].members){
+                if(room_info[i].members[j] === socket.id){
+                    //delete the member from the room
+                    console.log(i, j)
+                     room_info[i].members.splice(j, 1)
+                    //delete the room
+                    if(room_info[i].members.length === 0){
+                        room_info.splice(i, 1)
+                        isRoomDeleted = true
+                    }
+
+                    flag = true
+                    break
+                }
+            }
+            if(flag){
+                break
+            }
+        }
+
+        console.log(room_info)
+
+        io.emit('leave_room', {
+            room_id: parseInt(i)+1,
+            socket_id: socket.id,
+            isRoomDeleted: isRoomDeleted
+        })
+
     })
+
+    io.to(socket.id).emit('init_room_info', room_info)
 })
 
 module.exports = io;
